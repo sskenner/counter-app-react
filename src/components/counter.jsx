861ec48f93c94485c87efd9612f0e681aaa648ef
skA <sskenner@gmail.com>
2019-11-24 23:14:30 -0500

    summary

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,8 @@ class Counter extends Component {
     count: 0
   };
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -14,7 +15,7 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           increment
